fbb224355864443c208cc679e8dac80fbfd3c47a
Santiago <slongo2021@gmail.com>
2026-08-27 11:30:16 -0300

    Initial commit

---
+++ src/main/java/ar/edu/um/limitadorum/LimitadorumApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.um.limitadorum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitadorumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitadorumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/um/limitadorum/LimitadorumApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.um.limitadorum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitadorumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
